727e9ab93af6818c0cf322720072d620ecae2d4b
shiohone <bul1050@mail4.doshisha.ac.jp>
2014-08-24 19:10:15 +0900

    MainWindow.xaml.csが必要なくなったっぽいので全文コメントアウト

--- WPFTest01/MainWindow.xaml.cs
+++ WPFTest01/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -32,7 +32,7 @@ namespace WPFTest01
 
         }
     }
-}
+}*/
 
 //色の変更
 /*
